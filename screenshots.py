2de7ca335ae68067f66190dcc72c379f92ce20d4
jisbruzzi <jose.sbru@gmail.com>
2020-12-28 21:47:28 -0300

    2 screenshots

--- screenshots.py
+++ screenshots.py
@@ -5,21 +5,21 @@ from PIL import Image
 import PIL.ImageOps
 import subprocess
 
-def make_driver():
+def make_driver(url):
     options = Options()
     options.headless = True
     fp = webdriver.FirefoxProfile('/home/jose/.mozilla/firefox/5ud9v1om.default-release')
     driver = webdriver.Firefox(fp, options=options, executable_path="./geckodriver")
-    driver.get('https://ticktick.com/webapp/#q/all/tasks')
+    driver.get(url)
     driver.set_window_position(0, 0)
     driver.set_window_size(800, 1120)
     
     return driver
 
-def create_good_screenshot():
+def create_good_screenshot(left,right):
     background = Image.new('RGB', (1920, 1080), color = (255,255,255))
-    img = Image.open("screenshot.png").resize((int(800*1.5),int(1120*1.5)))
-    background.paste(img)
+    background.paste(Image.open(left).resize((int(800*1.2),int(1120*1.2))))
+    background.paste(Image.open(right).resize((int(800*1.2),int(1120*1.2))),(960,0))
     matrix = (
         0.6, 0, 0, 0,
         0, 0.6, 0, 0x2b,
@@ -35,12 +35,16 @@ def customize_page(driver):
 
 def update_desktop():
     subprocess.run(['feh', '--bg-scale', 'wallpaper.png'])
+    
 
-driver = make_driver()
+driver_week = make_driver("https://ticktick.com/webapp/#q/all/week")
+driver_tag = make_driver("https://ticktick.com/webapp/#t/b3BjaW9uYWw/tasks")
 while True:
     print("saving")
-    customize_page(driver)
-    screenshot = driver.save_screenshot('screenshot.png')
-    create_good_screenshot()
+    customize_page(driver_week)
+    driver_week.save_screenshot("week.png")
+    customize_page(driver_tag)
+    driver_tag.save_screenshot("optional.png")
+    create_good_screenshot("week.png","optional.png")
     update_desktop()
-    time.sleep(3)
+    
